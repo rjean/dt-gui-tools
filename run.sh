c594d187feb9d8bfbf92943bc5d1b071e9f59ef7
Andrea F. Daniele <afdaniele@ttic.edu>
2020-07-21 15:18:49 -0500

    removed run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-docker run -it \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --runtime=nvidia \
-    --net=ros \
-    -e ROS_MASTER_URI=http://proscore:11311 \
-    duckietown/dt-gui-tools:v1-amd64 \
-      bash
