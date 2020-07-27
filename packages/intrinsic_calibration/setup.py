0dbfa779771b81fb52101ac1ebe4b61e2324508c
Liam Paull <paulll@iro.umontreal.ca>
2020-07-27 17:40:18 -0400

    renaming to avoid name conflict

--- packages/intrinsic_calibration/setup.py
+++ packages/intrinsic_calibration/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['camera_calibration'],
+    packages=['intrinsic_calibration'],
 )
 
 setup(**setup_args)
